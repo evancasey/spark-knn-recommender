01c17a20eed8eb7b07173883ae9cdf7c517a63dd
Evan Casey <evan.casey92@gmail.com>
2014-04-09 22:14:21 -0700

    edit config with AWS dirs

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 CLUSTER_CONFIG = "local"
 
 # relative path
-PYSPARK_HOME = "../build/spark/pyspark"
-PYSPARK_MODULE_HOME = "../build/spark/python/pyspark"
-SPARKLER_HOME = "../build/spark/python/sparkler"
+PYSPARK_HOME = "../spark/pyspark"
+PYSPARK_MODULE_HOME = "../spark/python/pyspark"
+SPARKLER_HOME = "../spark/python/sparkler"
